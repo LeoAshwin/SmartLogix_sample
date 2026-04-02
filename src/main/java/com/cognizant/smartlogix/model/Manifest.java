a0dbe21ab2f8a1d9bec721183150456a22535c32
Leo Ashwin V <leo.ashwinv@cognizant.com>
2026-04-02 14:07:44 +0530

    Manifest module commit by leo

---
+++ src/main/java/com/cognizant/smartlogix/model/Manifest.java
@@ -0,0 +1,47 @@
+package com.cognizant.smartlogix.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Manifest") // Strictly matching doc
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Manifest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ManifestID")
+    private Long manifestId;
+
+    @Column(name = "DepotID")
+    private Long depotId;
+
+    @Column(name = "VehicleID")
+    private Long vehicleId;
+
+    @Column(name = "DriverID")
+    private Long driverId;
+
+    @Column(name = "Date")
+    private LocalDate date;
+
+    @Column(name = "StartAt")
+    private LocalDateTime startAt;
+
+    @Column(name = "EndAt")
+    private LocalDateTime endAt;
+
+    @Column(name = "StopsJSON", columnDefinition = "JSON")
+    private String stopsJson;
+
+    @Column(name = "Status")
+    private String status;
+}
